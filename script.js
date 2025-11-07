9f98eb9cc2b44e82a47cac8c2a535d034f9dd4ff
sadiksadov <saidkarimsaidmuhammedov56@gmail.com>
2025-11-07 10:58:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+// let age = prompt("yoshingizni kiriting")
+// let name = prompt("ismingizni kiriting")
+
+// alert(`Salom ${name}`)
+
+
+// if (age<18) {
+//     alert("siz voyaga yetmagan negrsiz")
+// }else {
+//     alert("siz voyga yetgan johny sinssiz")
+// }
+
+
+
+
+
+
+
+
+// const userName = prompt("Ismingizni kiriting:");
+// const userAge = prompt("Yoshingizni kiriting:");
+
+// console.log(`Foydalanuvchi: ${userName}, Yoshi: ${userAge}`);
+
+// alert(`Salom, ${userName}! Siz ${userAge} yoshdasiz.`);
+
+// if (userAge >= 18) {
+//     alert("Siz voyaga yetgansiz.");
+// } else {
+//     alert("Siz hali voyaga yetmagansiz.");
+// }
+
+// const user = {
+//     name: userName,
+//     age: Number(userAge),
+//     isAdult: userAge >= 18
+// };
+
+// let name = prompt("Ismingizni kiriting:").trim();
+// let birthYear = Number(prompt("Tug‘ilgan yilingizni kiriting:"));
+
+// let currentYear = new Date().getFullYear();
+
+// let age = currentYear - birthYear;
+
+// let userID = Math.floor(Math.random() * 10000);
+
+
+// let upperName = name.toUpperCase();
+
+
+// let category;
+// if (age < 12) {
+//     category = "bola";
+// } else if (age < 18) {
+//     category = "o‘smir";
+// } else if (age < 60) {
+//     category = "siz chiriyapsiz";
+// } else {
+//     category = "siz chirigansiz";
+// }
+
+
+// alert(`Salom, ${upperName}!\nSizning yoshingiz: ${age} yosh.\nToifangiz: ${category}.\nID raqamingiz: ${userID}`);
+
+
+
+
+
+let name = prompt("ismingiz:")
+let age = prompt("yosh:")
+let manxil = prompt("manzilni kiriting:")
+
+let id = Math.floor(Math.random() * 10000)
+
+const said ={
+
+    name: name,
+    age: age,
+    manxil: manxil,
+    id: id,
+}
+
+
+console.log(said);
+
+
+
+
+
